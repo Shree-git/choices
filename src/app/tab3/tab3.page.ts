984ba35234001724089af5d7fae230a33455dba9
Mirica Yancey <mirica.yancey@valpo.edu>
2020-03-24 17:22:23 -0500

    Rudimentary Calendar
    Added calendar without much interaction possible as a starter.

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { CalendarComponentOptions } from 'ion2-calendar';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +9,27 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+
+  optionsSingle: CalendarComponentOptions = {
+    color: 'danger'
+  };
+
+  dateRange: { from: string; to: string; };
+  type: 'string'; // 'string' | 'js-date' | 'moment' | 'time' | 'object'
+  optionsRange: CalendarComponentOptions = {
+    pickMode: 'range',
+    color: 'danger'
+  };
+
+  // Multi Select
+  dateMulti: string[];
+  optionsMulti: CalendarComponentOptions = {
+    pickMode: 'multi',
+    color: 'danger'
+  };
+  constructor( public platform: Platform) {}
+  onChange($event) {
+    console.log($event);
+  }
 
 }
